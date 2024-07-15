5d3c75bfe5148061644e3f7fa0a00c6efe6ebba4
emori <miurayuka.bee4@gmail.com>
2024-07-15 14:27:53 +0900

    vue.config.jsの変更

---
+++ docs/js/about.aeef4877.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunktest"]=self["webpackChunktest"]||[]).push([[594],{5377:function(n,t,u){u.r(t),u.d(t,{default:function(){return i}});var e=u(6768);const r={class:"about"},s=(0,e.Lk)("h1",null,"About",-1),l=(0,e.Lk)("p",null,null,-1),c=[s,l];function a(n,t){return(0,e.uX)(),(0,e.CE)("div",r,c)}var o=u(1241);const f={},k=(0,o.A)(f,[["render",a]]);var i=k}}]);
+//# sourceMappingURL=about.aeef4877.js.map
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,10 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   outputDir: "docs",
   publicPath: process.env.NODE_ENV === "production" ? "/test/" : "/",
   transpileDependencies: true,
-});
-module.exports = {
   devServer: {
     historyApiFallback: true,
   },
-};
+});
