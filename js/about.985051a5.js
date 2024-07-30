6dff8979a1495131f598c105dba03433bc253403
emori <miurayuka.bee4@gmail.com>
2024-07-30 14:58:23 +0900

    Updates

---
+++ js/about.985051a5.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunktest"]=self["webpackChunktest"]||[]).push([[594],{377:function(n,t,u){u.r(t),u.d(t,{default:function(){return i}});var e=u(641);const r={class:"about"},s=(0,e.Lk)("h1",null,"About",-1),l=(0,e.Lk)("p",null,null,-1),c=[s,l];function a(n,t){return(0,e.uX)(),(0,e.CE)("div",r,c)}var o=u(262);const f={},k=(0,o.A)(f,[["render",a]]);var i=k}}]);
+//# sourceMappingURL=about.985051a5.js.map
