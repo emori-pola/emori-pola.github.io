3895eb07104b6b99c28f30587c3393d1ebe3bf08
emori <miurayuka.bee4@gmail.com>
2024-09-02 13:45:59 +0900

    必要のないファイルの削除、パスの変更

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   outputDir: "docs",
-  publicPath: process.env.NODE_ENV === "production" ? "/test/" : "/",
+  publicPath: "/",
   transpileDependencies: true,
   devServer: {
     historyApiFallback: true,
