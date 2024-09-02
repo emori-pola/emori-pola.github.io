8615c8fa79bcc439d5b0c40cf899376c69eb3a21
emori <miurayuka.bee4@gmail.com>
2024-09-02 16:58:35 +0900

    404ページデザイン修正

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,8 +1,12 @@
 <template>
-  <div>
-    <h1>Page Not Found</h1>
-    <p>The page you are looking for does not exist.</p>
-    <router-link to="/">Go back to Home</router-link>
+  <div class="py-[120px]">
+    <h1 class="text-header02 mb-5">Page Not Found</h1>
+    <p class="mb-20">The page you are looking for does not exist.</p>
+    <router-link
+      class="inline-block border border-olive p-4 rounded-md mr-2 duration-300 ease-out hover:bg-olive hover:text-BG-primary"
+      to="/"
+      >Go back to Home</router-link
+    >
   </div>
 </template>
 
