76a9eeb6d4cb6c63fe0c802be0fc780a1062e54e
emori <miurayuka.bee4@gmail.com>
2024-09-02 16:53:23 +0900

    404ページ修正

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
 import CaseStudyView from "@/views/CaseStudyView.vue";
+import NotFound from "@/views/NotFound.vue";
 
 const routes = [
   {
@@ -20,6 +21,12 @@ const routes = [
     path: "/:id",
     name: "CaseStudy",
     component: CaseStudyView,
+  },
+  {
+    path: "/:catchAll(.*)",
+    name: "NotFound",
+    component: NotFound,
+    meta: { title: "Not Found" },
   },
 ];
 
