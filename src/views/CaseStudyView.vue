2f89efc5f6237bb35782ced0d36a6d9496085358
emori <miurayuka.bee4@gmail.com>
2024-09-05 11:14:13 +0900

    Case Studyの404ページ追加

--- src/views/CaseStudyView.vue
+++ src/views/CaseStudyView.vue
@@ -480,6 +480,15 @@
 
     <MoreWorks :excludeLink="caseStudy.id" />
   </div>
+  <div v-else class="py-[120px]">
+    <h1 class="text-header02 mb-5">Page Not Found</h1>
+    <p class="mb-20">The page you are looking for does not exist.</p>
+    <router-link
+      class="inline-block border border-olive p-4 rounded-md mr-2 duration-300 ease-out hover:bg-olive hover:text-BG-primary"
+      to="/"
+      >Go back to Home</router-link
+    >
+  </div>
 </template>
 
 <script>
